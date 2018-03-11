dcda9728a3268aae0469b1772d0eaf13e6b236b1
Angular CLI <angular-cli@angular.io>
2018-03-11 19:50:36 +0100

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scssss']
+})
+export class AppComponent {
+  title = 'app';
+}
