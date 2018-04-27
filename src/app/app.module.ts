92a447b2ad2e6ca4892205161ac0f4204bbcc8d5
Anders Skaatan <andersskatan@hotmail.com>
2018-04-27 15:03:27 +0200

    HTTP part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,17 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { HttpModule } from '@angular/http';
+
+import { baseURL } from './shared/baseurl';
+import { ProcessHttpmsgService } from './services/process-httpmsg.service';
 
 @NgModule({
   declarations: [
@@ -43,9 +47,12 @@ import { LoginComponent } from './login/login.component';
     FlexLayoutModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ DishService, PromotionService, LeaderService ],
+  providers: [ DishService, PromotionService, LeaderService,
+    { provide: 'BaseURL', useValue: baseURL },
+    ProcessHttpmsgService],
   entryComponents: [
     LoginComponent
   ],
